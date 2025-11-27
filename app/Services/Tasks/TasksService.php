24424b28128a024934b573a157f0ca58d816a696
Danillo Ferreira <nilloferreiira@gmail.com>
2025-11-27 17:56:26 -0300

    fix: updateBetweenLists and add compact

--- app/Services/Tasks/TasksService.php
+++ app/Services/Tasks/TasksService.php
@@ -83,17 +83,18 @@ class TasksService
             return response()->json(['message' => 'Lista não encontrada'], 404);
         }
 
+        if ($position < 0) $position = 0;
+
+        $maxPosition = Task::where('list_id', $listId)->max('position') ?? 0;
+        // Garante que o position nunca ultrapasse por mais de "1" o maximo da lista, assim a sequencia se mantem correta
+        if ($position > $maxPosition + 1) {
+            $position = $maxPosition + 1;
+        }
+
+        // dd($task, $listId, $position);
+
         DB::transaction(function () use ($task, $listId, $position) {
             // Update positions of other tasks in the target list
-            //TODO lidar com max position
-
-            if ($position > 0) $position = 0;
-
-            $maxPosition = Task::where('list_id', $listId)->max('position') ?? 0;
-            if ($position > $maxPosition + 1) {
-                $position = $maxPosition + 1;
-            }
-
             if ($task->list_id == $listId) {
                 $this->updatePositionsWithinList($task, $position);
                 $task->position = $position;
@@ -112,41 +113,44 @@ class TasksService
     public function updatePositionsBetweenLists(Task $task, $newListId, $newPosition)
     {
 
-        // Update positions in the old list
-        $this->updatePositionsWithinList($task, $newPosition);
+        $this->compactPositionsAfterRemove($task);
 
         // Update positions in the new list
-        if ($newPosition == $task->position) return response()->json(['message' => 'Ja esta na msm lista'], 200);
-
-        if ($newPosition < $task->position) {
-            Task::query()
-                ->where('list_id', $newListId)
-                ->whereBetween('position', [$newPosition, $task->position - 1])
-                ->increment('position');
-        } else {
-            Task::query()
-                ->where('list_id', $newListId)
-                ->whereBetween('position', [$task->position + 1, $newPosition])
-                ->decrement('position');
-        }
+        Task::query()
+            ->where('list_id', $newListId)
+            ->where('position', '>=', $newPosition)
+            ->increment('position');
     }
 
     public function updatePositionsWithinList(Task $task, $newPosition)
     {
-
-        if ($newPosition == $task->position) return;
-
         if ($newPosition < $task->position) {
             Task::query()
                 ->where('list_id', $task->list_id)
                 ->whereBetween('position', [$newPosition, $task->position - 1])
                 ->increment('position');
-        } else {
+        } else if ($newPosition > $task->position) {
             Task::query()
                 ->where('list_id', $task->list_id)
                 ->whereBetween('position', [$task->position + 1, $newPosition])
                 ->decrement('position');
+        } else {
+            Task::query()
+                ->where('list_id', $task->list_id)
+                ->where('position', '>=', $newPosition)
+                ->increment('position');
         }
+    }
+
+    public function compactPositionsAfterRemove(Task $task)
+    {
+        $tasks = Task::query()->where('list_id', $task->list_id)
+            ->where('id', '!=', $task->id)
+            ->where('position', '>', $task->position)
+            ->get();
+
+        $tasks->each->decrement('position');
+        // dd($tasks);
     }
 
     public function delete(User $user, $id)
